4603a8dafe42e86c5de106a733bd3cc377993bbc
Matheus Peviani Vellone <matheusvellone@hotmail.com>
2015-10-15 12:18:24 -0300

    Refazendo aplicativo - primeiro commit

---
+++ app/src/main/java/com/hofideas/cardapioruuel/utils/ConvertUtils.java
@@ -0,0 +1,11 @@
+package com.hofideas.cardapioruuel.utils;
+
+/**
+ * Created by Matheus on 15/10/2015.
+ */
+public class ConvertUtils {
+
+    public static String sigla(String string){
+        return string.substring(0, 3).toUpperCase();
+    }
+}
